67ffa3df2f47cd5c8523530f498c5a3fd5900a01
merhan-mohamed <39986371+merhan-mohamed@users.noreply.github.com>
2020-02-25 20:48:46 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,18 +1,32 @@
-from django.urls import path
+"""project URL Configuration
 
-from .views import product_list, product_details, index
-app_name = 'products'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', product_list,name='product'),
-    path('product_details/<slug:slug>/', product_details, name='product_details'),
-    path('index/', index, name='index'),
-
+    path('admin/', admin.site.urls),
+    path('product/', include('product.urls'), name='products'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('Cart/',include('django.contrib.auth.urls')),
+    path('Cart/', include('Cart.urls')),
 
 
 ]
-
-
-
-
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
